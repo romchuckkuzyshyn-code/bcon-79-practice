68785db0b67a333f334c31694244ff657f9553b2
Roman <romchuck.kuzyshyn@gmail.com>
2026-01-10 22:49:24 +0200

    ls2half

---
+++ module-01/lesson-02/script.js
@@ -0,0 +1,93 @@
+const a = 7;
+const b = 9;
+const ab = a * b;
+console.log("🚀 ~ ab:", ab);
+
+// 2
+
+const c = 7;
+const d = 9;
+const out2 = document.querySelector(".out-2");
+console.log("🚀 ~ out2:", out2);
+out2.textContent = c / d;
+
+// 3
+
+const e = 3;
+const f = 5;
+const out3 = document.querySelector(".out-3");
+console.log("🚀 ~ out3:", out3);
+out3.textContent = e + f;
+
+// 4
+
+const e1 = "3";
+const f1 = 5;
+const out4 = document.querySelector(".out-4");
+console.log("🚀 ~ out4:", out4);
+out4.textContent = e1 + f1;
+
+// 5
+
+const e2 = 3;
+const f2 = 0;
+const out5 = document.querySelector(".out-5");
+console.log("🚀 ~ out5:", out5);
+out5.textContent = e2 / f2;
+
+// 6
+
+const e3 = 3;
+const f3 = "Hello";
+const out6 = document.querySelector(".out-6");
+console.log("🚀 ~ out6:", out6);
+out6.textContent = e3 + f3;
+
+// 7
+
+const e4 = 3;
+const f4 = "Hello";
+const out7 = document.querySelector(".out-7");
+console.log("🚀 ~ out7:", out7);
+out7.textContent = e4 * f4;
+
+// 8
+
+const btn8 = document.querySelector(".b-8");
+const input8 = document.querySelector(".i-8");
+const out8 = document.querySelector(".out-8");
+
+btn8.onclick = fn8;
+
+function fn8() {
+  out8.textContent = input8.value;
+  input8.value = "";
+}
+
+// 9
+
+const input9 = document.querySelector(".i-9");
+const btn9 = document.querySelector(".b-9");
+const out9 = document.querySelector(".out-9");
+
+btn9.onclick = fn9;
+
+function fn9() {
+  out9.textContent = input9.value;
+  input9.value = "";
+}
+
+// 10
+
+const input10 = document.querySelector(".i-10");
+const btn10 = document.querySelector(".b-10");
+const out10 = document.querySelector(".out-10");
+
+btn10.onclick = fn10;
+
+function fn10() {
+  out10.textContent = input10.value * 20;
+  input10.value = "";
+}
+
+// 11
